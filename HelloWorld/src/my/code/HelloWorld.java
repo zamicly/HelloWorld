cc4d35d7a2d1eddf45ee0ceb754ad1b1da7a223a
Zane Clymer <zamicly@gmail.com>
2019-08-14 20:35:02 -0400

    Added Hello World code

---
+++ HelloWorld/src/my/code/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package my.code;
+
+/**
+ * Description
+ *
+ * @author Zane Clymer
+ * @date Aug 14, 2019
+ */
+public class HelloWorld {
+
+	/**
+	 * Main Method
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
